62884ec1ea39cac0578007523ccf8682c1ed0375
dummyGetUsernames <31829343+dummyGetUsernames@users.noreply.github.com>
2018-04-02 22:55:27 -0500

    my first WebScrapping
    This python script helps you scrap contents form a popular website NewEgg.com and scrap graphics cards details.
    Results will let you create a products.csv file which contains three attributes:
     a)Brand
     b) product name
     c)shipping cost

---
+++ first_webscrap.py
@@ -0,0 +1,41 @@
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+my_url= 'https://www.newegg.com/Product/ProductList.aspx?Submit=ENE&DEPA=0&Order=BESTMATCH&Description=vedio+cards&N=-1&isNodeId=1'
+
+#opening up the connection,grabbing the page requested
+uClient  = uReq(my_url)
+
+#get to store the page content to a variable
+page_html = uClient.read()
+
+uClient.close()
+#html parsinh
+page_soup = soup(page_html, "html.parser")
+
+#grabs each product by seeing inspect on each container
+containers = page_soup.findAll("div",{"class":"item-container"})
+
+filename = "products.csv"
+f = open(filename,"w")
+
+headers = "brand,product_name,shipping\n"
+f.write(headers)
+
+
+for container in containers:
+	brand = container.div.div.a.img["title"]
+
+	title_container = container.findAll("a",{"class":"item-title"})
+	product_name = title_container[0].text
+	shipping_container = container.findAll("li",{"class":"price-ship"})
+	shipping = shipping_container[0].text.strip()
+
+
+	print("brand: " +brand)
+	print("product_name: " +product_name)
+	print("shipping: " +shipping)
+
+	f.write(brand +","+ product_name.replace(",","|") +","+ shipping + "\n")
+f.close()
+
